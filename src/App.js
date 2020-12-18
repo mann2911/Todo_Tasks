1e36f744d29bc62a219674eaf7febf6ff9edcc45
Mann <mannpatel1360@gmail.com>
2020-12-18 14:37:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import Addtask from './Tasks/Tasks';
 
 class App extends Component{
   state={
-    inputarray:[],
+     inputarray:[],
      inputtext:'',
      showinput:true,
      inputindex:'',
-     editcheck:false
+     editcheck:false,
+     checkarray:[]
   }
   
   addtasksHandler=()=>{
 
     if(this.state.editcheck){
 
+      if(this.state.inputtext===''){
+        alert('Pl enter the tasks!!');
+      }
+
+      else if(this.state.checkarray.includes(this.state.inputindex)){
+        alert('u cannot edit tasks');
+      }
+
+      else{
       let name=this.state.inputtext;
       let index=this.state.inputindex;
       let copyinputarray=[
@@ -27,34 +37,71 @@ class App extends Component{
       copyinputarray[index]=obj;
       this.setState({inputarray:copyinputarray,editcheck:false})
       
-     
+      }
     }
-    else{
 
-      if(this.state.inputtext!=null){
-      let names=this.state.inputtext;
+    else{
+    
+      if(this.state.inputtext!==''){
+        let names=this.state.inputtext;
+        let copyinputarray=[
+         ...this.state.inputarray,
+        ]
+        let len=copyinputarray.length;
+        copyinputarray[len]=names;
+        this.setState({inputarray:copyinputarray})
+       }
+
+      else if(this.state.checkarray.includes(this.state.inputindex)){
+
+      alert('u cannot edit tasks');
+       }
+
+      else{
+      alert('Pl enter the Tasks');
+      }
+    }
+ }
+
+  checkboxfunction=(index)=>{
+      
       let copyinputarray=[
-        ...this.state.inputarray,
+       ...this.state.inputarray,
       ]
-      let len=copyinputarray.length;
-      copyinputarray[len]=names;
-      this.setState({inputarray:copyinputarray})
-    }
-    }
+      let name=copyinputarray[index];
+      let strike_name=this.strikeThrough(name);
+      copyinputarray[index]=strike_name;
+  
+      let copycheckarray=[ 
+        ...this.state.checkarray, 
+      ] 
+      let len=copycheckarray.length; 
+      copycheckarray[len]=index; 
+      this.setState({inputarray:copyinputarray,checkarray:copycheckarray})
     
     
+
   }
 
+   strikeThrough(text) {
+    return text
+      .split('')
+      .map(char => char + '\u0336')
+      .join('')
+  }
 
   editfunction=(index)=>{
 
+    if(this.state.checkarray.includes(index)){
+      alert('u cannot edit tasks');
+    }else{
     let copyinputarray=[
       ...this.state.inputarray,
     ]
-    console.log('index'+index);
+    
     let name=copyinputarray[index];
     this.setState({inputtext:name,editcheck:true,inputindex:index});
-    
+  }
   }
 
   deletefunction=(index)=>{
@@ -62,7 +109,10 @@ class App extends Component{
     let copyinputarray=[
       ...this.state.inputarray,
     ];
-    copyinputarray.splice(index,1);
+    if(this.state.checkarray.includes(index)){
+      alert('u cannot delete tasks');
+    }else{
+    copyinputarray.splice(index,1);}
     this.setState({
       inputtext:'',
       inputarray:copyinputarray
@@ -84,6 +134,7 @@ class App extends Component{
           return <Addtask
           editclick={()=>this.editfunction(index)}
           deleteclick={()=>this.deletefunction(index)}
+          checkboxclick={()=>this.checkboxfunction(index)}
           value={ch}
           key={index}/>
         })}
@@ -94,7 +145,7 @@ class App extends Component{
     
       <div className="App">
         
-        <h2>Add New Tasks Here: </h2>
+        <h2>Add Tasks Here: </h2>
        
         <input type="text"  onChange={this.add} value={this.state.inputtext}/>   
         <button  onClick={this.addtasksHandler}>Submit</button> 
